6ef63fef18ff97a8e2566f5d3779868bab33ec95
Alexis Maugain <alexis-maugain@users.noreply.github.com>
2026-01-07 21:43:26 +0100

    Configure deployment for GitHub Pages with landing page and game

--- landingPage/vite.config.js
+++ landingPage/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/ZippyTheMouse/',
     css: {
         postcss: './postcss.config.cjs', // Utilise le fichier PostCSS pour Tailwind
     },
